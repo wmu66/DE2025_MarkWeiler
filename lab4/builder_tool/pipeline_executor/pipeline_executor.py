f99a6aa8070a5de6c8201c673875719864c9083a
Indika PK  Weerasingha Dewage <ipkumarawd@yahoo.com>
2025-10-01 17:46:43 +0200

    Update pipeline_executor.py

--- lab4/builder_tool/pipeline_executor/pipeline_executor.py
+++ lab4/builder_tool/pipeline_executor/pipeline_executor.py
@@ -24,7 +24,7 @@ def run_pipeline_job(name, pipeline_def, pipeline_root, parameter_dict):
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', type=str, help="Pipeline Name")
-    parser.add_argument('--pipeline_def', type=str, default="pipeline.json", help="Pipeline JSON definition file")
+    parser.add_argument('--pipeline_def', type=str, default="pipeline.yaml", help="Pipeline YAML definition file")
     parser.add_argument('--pipeline_root', type=str, help="GCP bucket for pipeline_root")
     parser.add_argument('--parameter_dict', type=str, help="Pipeline parameters as a josn file")
     args = parser.parse_args()
